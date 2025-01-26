815d611e31f18625ccfc2e14073054da1f06a3bc
Mahmoud Mansy <mahmoudmohamedk8200949@gmail.com>
2025-01-26 17:23:27 +0200

    fix bugs in contact

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 const TopHeader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full bg-black z-40 px-2 md:px-4">
-      <div className="bg-black flex justify-between items-center lg:px-64">
+    <div className="fixed top-0 left-0 w-full bg-gray-900 z-40 px-2 md:px-4">
+      <div className="bg-gray-900 flex justify-between items-center lg:px-64">
         <div className="text-white flex justify-center gap-2 items-center flex-1">
           <h1 className="text-[11px] max-w-[200px] md:max-w-full md:text-sm">
             {i18n.t("topHeader")}
